1ea0336dab057fa8bddce7ec9afb134fbf5f3088
Muhammad Mubashir Mughal <donleo26@gmail.com>
2022-11-30 13:05:50 -0500

    added switch case for all buttons

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -21,8 +21,41 @@ public class MainActivity extends AppCompatActivity {
     public void numberEvent(View view) {
         String number = ed1.getText().toString();
         switch (view.getId()){
+            case R.id.bu1:
+                number += "1";
+                break;
+            case R.id.bu2:
+                number += "2";
+                break;
+            case R.id.bu3:
+                number += "3";
+                break;
+            case R.id.bu4:
+                number += "4";
+                break;
+            case R.id.bu5:
+                number += "5";
+                break;
+            case R.id.bu6:
+                number += "6";
+                break;
             case R.id.bu7:
                 number += "7";
+                break;
+            case R.id.bu8:
+                number += "8";
+                break;
+            case R.id.bu9:
+                number += "9";
+                break;
+            case R.id.bu0:
+                number += "0";
+                break;
+            case R.id.buDot:
+                number += ".";
+                break;
+            case R.id.buPlusMinus:
+                number += "-"+number;
                 break;
         }
         ed1.setText(number);
